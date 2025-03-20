1b35242c6c00e656bee1f3e0990d5fe57884fdcf
adjivas <adjivas@adjivas.eu>
2025-03-20 14:14:24 +0100

    test schema EIR

--- internal/sbi/api_sanity_test.go
+++ internal/sbi/api_sanity_test.go
@@ -7,26 +7,21 @@ import (
 	"testing"
 	"fmt"
 
-	"github.com/gin-gonic/gin"
 	"github.com/golang/mock/gomock"
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/require"
 
-	db "github.com/adjivas/eir/internal/database"
-	"github.com/adjivas/eir/internal/logger"
 	"github.com/adjivas/eir/pkg/factory"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/adjivas/eir/internal/logger"
 	util_logger "github.com/free5gc/util/logger"
-	"github.com/free5gc/util/mongoapi"
 
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 
 )
-
-type testdata struct {
-	influId string
-	supi    string
-}
 
 func setupHttpServer(t *testing.T) *gin.Engine {
 	router := util_logger.NewGinWithLogrus(logger.GinLog)
@@ -56,15 +51,61 @@ func setupHttpServer(t *testing.T) *gin.Engine {
 	return router
 }
 
-func setupMongoDB(t *testing.T) {
-	err := mongoapi.SetMongoDB("test5gc", "mongodb://localhost:27017")
-	require.Nil(t, err)
-	err = mongoapi.Drop(db.APPDATA_INFLUDATA_DB_COLLECTION_NAME)
-	require.Nil(t, err)
-	err = mongoapi.Drop(db.APPDATA_INFLUDATA_SUBSC_DB_COLLECTION_NAME)
-	require.Nil(t, err)
-	err = mongoapi.Drop(db.APPDATA_PFD_DB_COLLECTION_NAME)
-	require.Nil(t, err)
+func setupMongoDB() (*mongo.Database) {
+	uri := "mongodb://localhost:27017"
+ 
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	// Send a ping to confirm a successful connection
+	var result bson.M
+	db := client.Database("free5gc")
+	if err := db.RunCommand(context.TODO(), bson.D{{"ping", 1}}).Decode(&result); err != nil {
+		panic(err)
+	}
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+	return db
+}
+
+func createEquipementStatus(db *mongo.Database) {
+	// Create Collection
+	jsonSchema := bson.M{
+		"bsonType": "object",
+		"required": []string{"pei", "equipement_status"},
+		"properties": bson.M{
+			"pei": bson.M{
+				"bsonType": "string",
+				"pattern": "^(imei-[0-9]{15}|imeisv-[0-9]{16}|mac((-[0-9a-fA-F]{2}){6})(-untrusted)?|eui((-[0-9a-fA-F]{2}){8}))$",
+				"description": "Data type representing the PEI of the UE",
+			},
+			"supi": bson.M{
+				"bsonType": "string",
+				"pattern": "^(imsi-[0-9]{5,15}|nai-.+|gci-.+|gli-.+)$",
+				"description": "Data type representing the SUPI of the subscriber",
+			},
+			"gpsi": bson.M{
+				"bsonType": "string",
+				"pattern": "^(msisdn-[0-9]{5,15}|extid-[^@]+@[^@]+)$",
+				"description": "Data type representing the GPSI of the subscriber",
+			},
+			"equipement_status": bson.M{
+				"bsonType": "string",
+				"enum": []string{"WHITELISTED", "BLACKLISTED", "GREYLISTED"},
+				"description": "Indicates the PEI is white, black or grey listed",
+			},
+		},
+	}
+	validator := bson.M{
+		"$jsonSchema": jsonSchema,
+	}
+	opts := options.CreateCollection().SetValidator(validator)
+
+	err := db.CreateCollection(context.TODO(), "policyData.ues.eirData", opts)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func TestEIR_Root(t *testing.T) {
@@ -102,67 +143,72 @@ func TestEIR_EquipementStatus_NotFound(t *testing.T) {
 	// })
 }
 
-func TestEIR_EquipementStatus_Adjivas(t *testing.T) {
-	uri := "mongodb://localhost:27017"
-	// Use the SetServerAPIOptions() method to set the Stable API version to 1
+func TestEIR_EquipementStatus_DataBaseInsert(t *testing.T) {
+	db := setupMongoDB()
 
-	// Create a new client and connect to the server
-	client, err := mongo.Connect(options.Client().ApplyURI(uri))
-	if err != nil {
-		panic(err)
-	}
+	createEquipementStatus(db)
+
 	defer func() {
-		if err = client.Disconnect(context.TODO()); err != nil {
+		if err := db.Collection("policyData.ues.eirData").Drop(context.TODO()); err != nil {
 			panic(err)
 		}
 	}()
-	// Send a ping to confirm a successful connection
-	var result bson.M
-	db := client.Database("free5gc")
-	if err := db.RunCommand(context.TODO(), bson.D{{"ping", 1}}).Decode(&result); err != nil {
-		panic(err)
-	}
-	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
 
-	// Create Collection
-	jsonSchema := bson.M{
-		"bsonType": "object",
-		"required": []string{"name", "age"},
-		"properties": bson.M{
-			"name": bson.M{
-				"bsonType": "string",
-				"enum": []string{"alex", "la"},
-				"description": "the name of the user, which is required and " +
-					"must be a string",
-			},
-			"age": bson.M{
-				"bsonType": "int",
-				"minimum":  18,
-				"description": "the age of the user, which is required and " +
-					"must be an integer >= 18",
-			},
-		},
-	}
-	validator := bson.M{
-		"$jsonSchema": jsonSchema,
-	}
-	opts := options.CreateCollection().SetValidator(validator)
+	// Success to Insert documents
+	// With valid pei and equipement_status
+	res, err := db.Collection("policyData.ues.eirData").InsertOne(context.TODO(), bson.M{
+		"pei": "imei-012345678901234",
+		"equipement_status": "WHITELISTED",
+	})
+	assert.Nil(t, err)
+	assert.NotNil(t, res)
+	// With valid pei, equipement_status, supi and gpsi
+	res, err = db.Collection("policyData.ues.eirData").InsertOne(context.TODO(), bson.M{
+		"pei": "imei-012345678901234",
+		"supi": "imsi-208930000000001",
+		"gpsi": "msisdn-00000",
+		"equipement_status": "WHITELISTED",
+	})
+	assert.Nil(t, err)
+	assert.NotNil(t, res)
 
-	err = db.CreateCollection(context.TODO(), "policyData.ues.eirData", opts)
-	if err != nil {
-		panic(err)
-	}
-
-	// Insert One Document
-	res, err := db.Collection("policyData.ues.eirData").InsertOne(context.TODO(), bson.M{"name": "alex", "age": 31})
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("inserted document with ID %v\n", res.InsertedID)
-
-	// Drop Collection
-	err = db.Collection("policyData.ues.eirData").Drop(context.TODO())
-	if err != nil {
-		panic(err)
-	}
+	// Fail to Insert documents
+	// With missing Pei
+	res, err = db.Collection("policyData.ues.eirData").InsertOne(context.TODO(), bson.M{
+		"equipement_status": "BLACKLISTED",
+	})
+	assert.NotNil(t, err)
+	assert.Nil(t, res)
+	// With missing equipement_status
+	res, err = db.Collection("policyData.ues.eirData").InsertOne(context.TODO(), bson.M{
+		"pei": "imei-012345678901234",
+	})
+	assert.NotNil(t, err)
+	assert.Nil(t, res)
+	// With missing Pei and equipement_status
+	res, err = db.Collection("policyData.ues.eirData").InsertOne(context.TODO(), bson.M{
+	})
+	assert.NotNil(t, err)
+	assert.Nil(t, res)
+	// With not valid Pei
+	res, err = db.Collection("policyData.ues.eirData").InsertOne(context.TODO(), bson.M{
+		"pei": "ppppppppppppp-012345678901234",
+		"equipement_status": "BLACKLISTED",
+	})
+	// With not valid equipement_status
+	res, err = db.Collection("policyData.ues.eirData").InsertOne(context.TODO(), bson.M{
+		"pei": "imei-012345678901234",
+		"equipement_status": "PINKLISTED",
+	})
+	assert.NotNil(t, err)
+	assert.Nil(t, res)
+	// With not valid supi and gpsi
+	res, err = db.Collection("policyData.ues.eirData").InsertOne(context.TODO(), bson.M{
+		"pei": "imei-012345678901234",
+		"supi": "sssssssssss",
+		"gpsi": "ggggggggggg",
+		"equipement_status": "BLACKLISTED",
+	})
+	assert.NotNil(t, err)
+	assert.Nil(t, res)
 }
