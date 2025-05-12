71268aa220b229006dd50176b34fbb4fa4a049e3
adjivas <adjivas@adjivas.eu>
2025-05-12 14:10:36 +0200

    Remove EirDefaultKeyLogPath duplicata of EirDefaultTLSKeyLogPath

--- internal/util/path.go
+++ internal/util/path.go
@@ -1,7 +1,6 @@
 package util
 
 var (
-	EirDefaultKeyLogPath = "./log/eirsslkey.log"
 	EirDefaultPemPath    = "./config/TLS/eir.pem"
 	EirDefaultKeyPath    = "./config/TLS/eir.key"
 	EirDefaultConfigPath = "./config/eircfg.yaml"
