9f9fe6558269ee983be47b904aa1cc03f2d1ea0f
adjivas <adjivas@adjivas.eu>
2025-06-12 18:35:19 +0200

    fix: ci

--- internal/sbi/api_sanity_test.go
+++ internal/sbi/api_sanity_test.go
@@ -20,7 +20,7 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
 func setupHttpServer(t *testing.T) *gin.Engine {
